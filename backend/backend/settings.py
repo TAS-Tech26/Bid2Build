ec4a1e93dc4b5b59c5b598d10dfd310e9f511251
Srimannarayan Paidisetty <78963283+SrimannarayanP@users.noreply.github.com>
2026-05-18 00:18:30 +0530

    Refactored settings.py to use environ for config
    Updated settings to use env vars for config.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,18 +12,24 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+import environ, os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env(DEBUG = (bool, False))
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-45i_!hx9jdre!&1emktv(g7br=(7wykrb8belbas7pw(b55pr@'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -77,11 +83,11 @@ ASGI_APPLICATION = 'backend.asgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'b2b',
-        'USER' : 'Sriman',
-        'PASSWORD' : 'SpitterDune#891*',
-        'HOST' : '127.0.0.1',
-        'PORT' : '5432',
+        'NAME': env('DB_NAME'),
+        'USER' : env('DB_USER'),
+        'PASSWORD' : env('DB_PASSWORD'),
+        'HOST' : env('DB_HOST'),
+        'PORT' : env('DB_PORT'),
     }
 }
 
