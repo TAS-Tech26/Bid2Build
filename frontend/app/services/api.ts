1edcb558b2235bdad8a3b61a77239cd66c384220
PraveenDV <69348358+PraveenDV@users.noreply.github.com>
2026-08-23 00:51:37 +0530

    Add files via upload

--- frontend/app/services/api.ts
+++ frontend/app/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api=axios.create({
-    baseURL:"http://127.0.0.1:8000/api/",
+    baseURL:"http://127.0.0.1:8002/",
 });
 
 api.interceptors.request.use(
@@ -14,7 +14,7 @@ api.interceptors.request.use(
             if (token) {
 
                 config.headers.Authorization =
-                    `Token ${token}`;
+                    `Bearer ${token}`;
 
             }
         }
